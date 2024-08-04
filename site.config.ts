73c1cf6304026af8ea6d9df8caae5a1e7a2939d7
Enterface <94808568+EnterfaceLLC@users.noreply.github.com>
2024-08-04 14:37:40 -0500

    Update site.config.ts
    my id page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e466fc0df81f4766a8d4fe7716d67c7b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
