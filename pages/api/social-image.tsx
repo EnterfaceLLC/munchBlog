43841e1002e35cdb7559a0d1c6706e3811359f62
EnterfaceLLC <dotenterface@gmail.com>
2024-08-10 00:33:34 -0500

    bold fonts

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -163,13 +163,13 @@ export default async function OGImage(req: NextRequest) {
           data: interRegularFont,
           style: 'normal',
           weight: 400
-        },
-        {
-          name: 'Inter',
-          data: interBoldFont,
-          style: 'normal',
-          weight: 700
         }
+        // {
+        //   name: 'Inter',
+        //   data: interBoldFont,
+        //   style: 'normal',
+        //   weight: 700
+        // }
       ]
     }
   )
